af03aaaa8286dfd91c90ab0e8ea225e5cf19f540
Eitan Traurig <etraurig@mail.yu.edu>
2023-05-01 17:09:36 -0400

    Add enum to MaintnaceJob. Add getters and setters

--- src/main/java/edu/yu/cs/com1320/project/MaintnenceJob.java
+++ src/main/java/edu/yu/cs/com1320/project/MaintnenceJob.java
@@ -4,13 +4,52 @@ import java.util.*;
 
 public class MaintnenceJob {
     List<Mechanic> mechanics;
-    void Enum{}//fixme
+    public enum maintananceTypes {OIL,Brakes,Tires}
     double odometer;
     String description;
     Date date;
     double price;
     boolean paid;
 
+
+    //Getters
+    public List<Mechanic> getMechanics(){
+        return new ArrayList<>(mechanics);
+    }
+    public String getDescription(){
+        return description;
+    }
+    public Data getDate(){
+        return date;
+    }
+    public double getOdometer(){
+        return odometer;
+    }
+    public  double getPrice(){
+        return price;
+    }
+    public boolean wasPaid(){
+        return paid;
+    }
+    //Setters
+    public void addMechanic(Mechanic mechanic){
+
+    }
+    public void setDescription(String description){
+        this.description =description;
+    }
+    public void setDate(Date date){
+        this.date = date;
+    }
+    public void setOdometer(double odometer){
+        this.odometer = odometer;
+    }
+    public  void setPrice(double price){
+        this.price = price;
+    }
+    public void setPaid(boolean paid){
+        this.paid = paid;
+    }
 
 
 }
