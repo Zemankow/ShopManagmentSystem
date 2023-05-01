f7852e3a4339d66eb588b2309272544564442d1d
Eitan Traurig <etraurig@mail.yu.edu>
2023-05-01 17:00:10 -0400

    replace arrays with Lists

--- src/main/java/edu/yu/cs/com1320/project/Car.java
+++ src/main/java/edu/yu/cs/com1320/project/Car.java
@@ -1,19 +1,21 @@
 package edu.yu.cs.com1320.project;
 
 import java.util.Date;
+import java.util.List;
 
 public class Car {
     String vin, make, model;
     Date year;
     double odometer;
-    MaintnenceJob[] previous, current, recomended;//todo make list
+    List<MaintnenceJob> previous, current, recomended;//todo make list
 
     public Car(){
         //fixme
     }
 
-    private
-    List<MaintnenceJob>
+    private List<MaintnenceJob> getRecomendedMaintanancec(){
+        return null;
+    }
 
 
 }
--- src/main/java/edu/yu/cs/com1320/project/Shop.java
+++ src/main/java/edu/yu/cs/com1320/project/Shop.java
@@ -1,4 +1,12 @@
 package edu.yu.cs.com1320.project;
 
+import java.util.List;
+import java.util.Queue;
+
 public class Shop {
+
+    List<Car> bay;
+    Queue<Car> requiresWork;
+    List<Car> readyForPickup;
+    List<Customer> customers;
 }
